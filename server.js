cbcb2cb7bf4f1a2f050231d46e0bd3d2dee299e5
izaacAbrams <izaac.abrams25@gmail.com>
2020-03-01 21:09:54 -0600

    added endpoint with search params

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const MOVIES = require("./movies-data-small.json");
+const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+
+require("dotenv").config();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+  const authToken = req.get("Authorization");
+  const apiToken = process.env.API_TOKEN;
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.get("/movie", handleGetMovie);
+
+function handleGetMovie(req, res) {
+  response = MOVIES;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movie => movie.avg_vote >= Number(req.query.avg_vote)
+    );
+  }
+  res.json(response);
+}
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
